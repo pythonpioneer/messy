674ed35d03fc78940bc583375436f73a4dedcd15
Hritik Kumar Sinha <85961247+pythonpioneer@users.noreply.github.com>
2025-09-10 23:32:59 +0530

    rollup added

---
+++ rollup-config.mjs
@@ -0,0 +1,44 @@
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import preserveShebang from 'rollup-plugin-preserve-shebang';
+
+export default {
+    input: 'src/index.ts',
+    output: {
+        file: 'dist/index.js',
+        format: 'cjs', // CLI for Node
+        banner: '#!/usr/bin/env node',
+    },
+    plugins: [
+        preserveShebang(),
+        nodeResolve({ preferBuiltins: true }),
+        commonjs(),
+        json(),
+        typescript({ tsconfig: './tsconfig.json', declaration: false }), // no .d.ts
+    ],
+    external: [
+        'fs',
+        'path',
+        'os',
+        'crypto',
+        'child_process',
+        'util',
+        'stream',
+        'readline',
+        'events',
+        'assert',
+        'tty',
+        'zlib',
+        'url',
+        'http',
+        'https',
+        'net',
+        'tls',
+        'perf_hooks',
+        // leave commander external if you want smaller bundles
+        // otherwise Rollup will include it
+        'commander',
+    ],
+};
